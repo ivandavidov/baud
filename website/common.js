4d65f33adba71a704f4bf70be19c260a10fd1b09
Ivan Davidov <davidov.i@gmil.com>
2017-04-05 22:39:55 +0300

    Website layout updates.

--- website/common.js
+++ website/common.js
@@ -1,44 +1,70 @@
 var trace1 = {
   mode: 'lines+markers',
-  name: 'Начало',
+  name: 'Дялове',
   x: [], y: []
 };
 
 var trace2 = {
   mode: 'lines+markers',
-  name: 'Край',
+  name: 'Цена',
+  yaxis: 'y2',
   x: [], y: []
 };
 
 var data = [trace1, trace2];
-data = [trace2];
 
 var layout = {
   title: 'Договорен Фонд',
-  __xaxis: {
-    title: 'Седмица'
+  titlefont: {
+      size: 22
   },
-  __yaxis: {
-    title: 'Дялове'
+  xaxis: {
+    title: 'Дата',
+    titlefont: {
+      size: 18
+    }
   },
+  yaxis: {
+    title: 'Дялове',
+    titlefont: {
+      size: 18
+    }
+  },
+  yaxis2: {
+    title: 'Цена',
+    titlefont: {
+      size: 18
+    },
+    overlaying: 'y',
+    side: 'right',
+    showgrid: false
+  },
+  showlegend: true,
   legend: {
-    orientation: 'h'
+    orientation: 'h',
+    x: 0.01,
+    y: -0.09
   }
 }
 
-layout.title = 'Дялове на ' + o.f + ' от ' + o.e[0].d + ' до ' + o.e[o.e.length - 1].d
+layout.title = 'Статистика за ' + o.f + ' от ' + o.e[0].d + ' до ' + o.e[o.e.length - 1].d
 document.title = layout.title;
 
 var plotlyBar = {
-  displayModeBar: true
+  displayModeBar: true,
+  displaylogo: false
 }
 
 for(var i = 0; i < o.b.length; i++) {
-  trace1.x.push(o.b[i].d);
-  trace1.y.push(o.b[i].s);
+  trace1.x.push(o.e[i].d);
+  trace1.y.push(o.e[i].s);
 }
 
 for(var i = 0; i < o.e.length; i++) {
   trace2.x.push(o.e[i].d);
-  trace2.y.push(o.e[i].s);
+  trace2.y.push(o.e[i].p);
+}
+
+function replot() {
+  Plotly.newPlot('linesDiv', data, layout, plotlyBar);
 }

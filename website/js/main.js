05f48bf5f840cbc757b3f4f5f5185884a050acbb
Ivan Davidov <davidov.i@gmil.com>
2017-04-09 20:34:04 +0300

    Website improvement - added enhancement for pretty URLs.

--- website/js/main.js
+++ website/js/main.js
@@ -1,6 +1,22 @@
-function menu(jsPart) {
+function menu(href) {
+  document.location.href=href;
+  load();
+}
+
+function load() {
+  var hrefPart = document.location.href.split('#')[1];
+  
   var chart = document.getElementById('chart');
   chart.style.display = 'block';
-  chart.src = 'fund.html?' + jsPart;
-  return false;
+  chart.src = 'fund.html?' + hrefPart;
+}
+
+function defaultMenu(item) {
+  var href = document.location.href.split('#')[1];
+  
+  if(href == undefined) {
+    href = item;
+  }
+
+  menu('#' + href);
 }
--- website_updater/update.sh
+++ website_updater/update.sh
@@ -2,18 +2,17 @@
 
 cd /home/ivan/projects/baud/website_updater
 
-./update_one.sh 9 dsk_b
-./update_one.sh 10 dsk_r
-./update_one.sh 15 elana_b_e
-./update_one.sh 131 elana_b
-./update_one.sh 19 capman_c
-./update_one.sh 20 capman_m
-./update_one.sh 22 advans_i_e
-./update_one.sh 21 advans_i
-./update_one.sh 27 ubb_b
-./update_one.sh 28 ubb_p_a
-./update_one.sh 75 ccb_a
-./update_one.sh 55 ccb_l
-./update_one.sh 116 ug_m
-./update_one.sh 117 ug_o
-
+./update_one.sh 9 dsk-balans
+./update_one.sh 10 dsk-rastej
+./update_one.sh 15 elana-balansiran-evro-fond
+./update_one.sh 131 elana-bulgaria
+./update_one.sh 19 kapman-kapital
+./update_one.sh 20 kapman-maks
+./update_one.sh 22 advans-iztochna-evropa
+./update_one.sh 21 advans-invest
+./update_one.sh 27 obb-balansiran-fond
+./update_one.sh 28 obb-premium-akcii
+./update_one.sh 75 ckb-aktiv
+./update_one.sh 55 ckb-lider
+./update_one.sh 116 iug-market-maksimum
+./update_one.sh 117 iug-market-optimum

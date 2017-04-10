48d6e94e3e5a94803d687da0501dc568288ee015
Ivan Davidov <davidov.i@gmil.com>
2017-04-10 21:04:15 +0300

    Added website functionality to download raw CSV data.

--- website_updater/update_one.cmd
+++ website_updater/update_one.cmd
@@ -5,15 +5,37 @@ cd ..
 if "%1" == "" goto missing_param
 if "%2" == "" goto missing_param
 
+if exist daily.csv del daily.csv
+if exist weekly.csv del weekly.csv
 if exist weekly.js del weekly.js
+
 call run %1
-if not exist weekly.js goto missing_js
+
+if not exist daily.csv goto missing_daily_csv
+if not exist weekly.csv goto missing_weekly_csv
+if not exist weekly.js goto missing_weekly_js
+
+copy /y daily.csv website\data\%2-dnevni-danni.csv
+echo The file 'website\data\%2-dnevni-danni.csv' has been processed.
+
+copy /y weekly.csv website\data\%2-sedmichni-danni.csv
+echo The file 'website\data\%2-sedmichni-danni.csv' has been processed.
+
 copy /y weekly.js website\data\%2.js
 echo The file 'website\data\%2.js' has been processed.
+
 goto exit
 
-:missing_js
+:missing_weekly_js
 echo The file 'weekly.js' is missing.
+goto exit
+
+:missing_daily_csv
+echo The file 'daily.csv' is missing.
+goto exit
+
+:missing_weekly_csv
+echo The file 'weekly.csv' is missing.
 goto exit
 
 :missing_param
--- website_updater/update_one.sh
+++ website_updater/update_one.sh
@@ -15,17 +15,34 @@ if [ "$2" = "" ] ; then
   exit 1
 fi;
 
-rm weekly.js
+rm daily.csv
+rm weekly.csv
+rm daily.csv
 
 ./run.sh $1
 
-if [ ! -f "weekly.js" ] ; then
+if [ ! -f daily.csv ] ; then
+  echo "The file 'daily.csv' is missing."
+  exit 1
+fi;
+
+if [ ! -f weekly.csv ] ; then
+  echo "The file 'weekly.csv' is missing."
+  exit 1
+fi;
+
+if [ ! -f weekly.js ] ; then
   echo "The file 'weekly.js' is missing."
   exit 1
 fi;
 
-cp weekly.js website/data/$2.js
+cp daily.csv website/data/$2-dnevni-danni.csv
+echo "The file 'website/data/$2-dnevni-danni.csv' has been processed."
 
+cp weekly.csv website/data/$2-sedmichni-danni.csv
+echo "The file 'website/data/$2-sedmichni-danni.csv' has been processed."
+
+cp weekly.js website/data/$2.js
 echo "The file 'website/data/$2.js' has been processed."
 
 cd website_updater

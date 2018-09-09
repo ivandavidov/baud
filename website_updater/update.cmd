7f2a05e6e304e55345af985018e168a1b01d0f24
Ivan Davidov <davidov.i@gmil.com>
2018-09-09 17:40:02 +0300

    Latest 'plotly' library && added few more mutual funds.

--- website_updater/update.cmd
+++ website_updater/update.cmd
@@ -1,16 +1,24 @@
 @echo off
 
+call update_one 12 alfa-sofix-index
 call update_one 9 dsk-balans
 call update_one 10 dsk-rastej
+call update_one 8 dsk-standart
+call update_one 134 expat-bulgaria-sofix-ucits-etf
 call update_one 15 elana-balansiran-evro-fond
 call update_one 131 elana-bulgaria
+call update_one 87 elana-evrofond
+call update_one 42 elana-fond-svobodni-pari
+call update_one 65 zlaten-lev-index-30
 call update_one 19 kapman-kapital
 call update_one 20 kapman-maks
 call update_one 22 advans-iztochna-evropa
 call update_one 21 advans-invest
 call update_one 27 obb-balansiran-fond
+call update_one 26 obb-platinum-obligacii
 call update_one 28 obb-premium-akcii
 call update_one 75 ckb-aktiv
+call update_one 76 ckb-garant
 call update_one 55 ckb-lider
 call update_one 116 iug-market-maksimum
 call update_one 117 iug-market-optimum
